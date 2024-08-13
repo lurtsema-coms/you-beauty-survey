b2cc189a5f8f0c1e40a0b9df5efd466e38b2e0dd
Fil <filongunio@gmail.com>
2024-08-13 08:42:40 +0800

    create migration and model for survey, choice, and question

---
+++ database/migrations/2024_08_13_002951_create_choices_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('question_id')->references('id')->on('questions');
+            $table->string('choice');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('choices');
+    }
+};

536751395f8b844e6502da8c9a8ef846fe3cc00a
Fil <filongunio@gmail.com>
2024-08-15 01:24:47 +0800

    add migration and model for form response and form response answer

---
+++ app/Models/FormResponseAnswer.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormResponseAnswer extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+}
---
+++ database/migrations/2024_08_14_171132_create_form_response_answers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\FormResponse;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('form_response_answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(FormResponse::class);
+            $table->string('question');
+            $table->text('value')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('form_response_answers');
+    }
+};

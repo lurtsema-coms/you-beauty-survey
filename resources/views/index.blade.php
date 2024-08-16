5a6af92ac890cc88821b0b669ba1ed457a16d5b0
Fil <filongunio@gmail.com>
2024-08-17 00:31:55 +0800

    update modal content

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -75,10 +75,10 @@
                     <svg class="w-12 h-12 mx-auto mb-4 text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
                     </svg>
-                    <h3 class="text-lg font-normal text-gray-500">Submit this response?</h3>
+                    <h3 class="text-lg font-normal text-gray-500">Are you sure you want to submit this response?</h3>
                     <p class="mb-5 italic text-gray-500">You won't be able to revert this action.</p>
                     <button data-modal-hide="popup-modal" type="button" class="text-white bg-green-600 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center" onclick="submitForm()">
-                        Yes, I'm sure
+                        Yes
                     </button>
                     <button data-modal-hide="popup-modal" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100">No, cancel</button>
                 </div>
